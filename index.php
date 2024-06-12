4e886be5345c68389f3c5de3ad0821f6c6790d4c
erick643 <erickosornio437@gmail.com>
2024-06-12 16:08:49 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta name="view port" content="width=device-width, initial-scale=1.0">
+	<title> PAPELERIA</title>
+	
+</head>
+<body>
+<div class="login-box">
+	<form  action="" method="POST">
+		
+			<p style="color:black;text-align: center; font-size:50px;font: bold 150% monospace;">
+				<br>Registro De<br/>
+			Papeleria</br>
+			</p>		
+			
+			
+			
+		
+		<label for="">ID: </label>
+		<input type="text" name="id" class="Barra"><br>
+		
+		<label> Nombre Del Producto: </label>
+		<input type="text" name="nombre"class="Barra"><br>
+		
+		<label> Precio: </label>
+		<input type="text" name="precio"class="Barra"><br>
+		
+		<label> Descripcion: </label>
+		<input type="text" name="descripcion"class="Barra"><br>
+		
+		<label> Lote: </label>
+		<input type="text" name="lote"class="Barra"><br>
+		
+		<label> Provedor: </label>
+		<input type="text" name="proveedor"class="Barra"><br><hr>
+		
+		<input type="submit" name="insertar" value="INSERTAR"class="button">
+		<input type="submit" name="consultar" value="CONSULTAR" class="button">
+		<input type="submit" name="modificar" value="MODIFICAR" class="button">
+		<input type="submit" name="eliminar" value="ELIMINAR"class="button">
+		
+	</form>
+
+
+</div>
+
+<div class="slider-box">
+    <ul>
+        <li>
+            <img class="imagen" src="img\img1.jpeg" alt="">
+        </li>
+		<li>
+            <img class="imagen" src="img\img2.jpeg" alt="">
+        </li>
+		<li>
+            <img class="imagen" src="img\img3.jpeg" alt="">
+        </li>
+		<li>
+            <img class="imagen" src="img\img4.jpeg" alt="">
+        </li>
+
+    </ul>
+</div>	
+
+	<footer class="pie-pagina">
+	
+		<div class="grupo-2">
+			
+			<p style="color:black;text-align: center; font-size:30px">
+				<br>---Siguenos--</br>
+			</p>
+			<a href="https://www.facebook.com/">
+					<img src="img\faceb.png" alt="Face">
+				</a>
+				<a href="https://www.instagram.com/">
+					<img src="img\insta.png" alt="insta">
+				</a>
+				<a href="https://x.com/x.">
+					<img src="img\X.png" alt="X">
+				</a>
+			
+				<p style="color:black;text-align: center; font-size:20px">
+				<br>---Contactanos: 7122516153---
+				
+				<br>---Correo: <a href="https://www.google.com">PapeleriaDcm@gmail.com</a>---
+				</p>
+						<div id="mapa">
+							<h1>---UBICACION---</h1>
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d487.5911413537653!2d-99.82746546260091!3d20.09790240714747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d3ab76cc756cb3%3A0x941691f58141eb46!2sPapeleria%2C%20Merceria%20y%20Regalos%20del%20Carmen!5e0!3m2!1ses-419!2smx!4v1717104811683!5m2!1ses-419!2smx"
+								width="500" height="225" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+						</div>
+            <p style="color:black;text-align: center; font-size:20px">
+				<small>&copy; 2024 <b>ERICK</b> - Todos Los Derechos Reservados.</small><br>
+			</p>						
+		</div>
+	</footer>	
+  </body>
+</html>
